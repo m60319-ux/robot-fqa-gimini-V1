6b05a1d315336cce4dd0e481b8dc4f594bd0c782
m60319-ux <m60319@gmail.com>
2026-01-30 16:16:30 +0800

    Add Chinese FAQ data to data.th.js

--- assets/data/data.th.js
+++ assets/data/data.th.js
@@ -1,1 +1,27 @@
-
+window.FAQ_DATA_ZH = {
+    "meta": { "lang": "zh", "version": "1.0" },
+    "categories": [
+        {
+            "id": "CAT-01",
+            "title": "硬體問題",
+            "subcategories": [
+                {
+                    "id": "SUB-01",
+                    "title": "馬達異常",
+                    "questions": [
+                        {
+                            "id": "Q-001",
+                            "title": "無法過電",
+                            "content": {
+                                "symptoms": ["燈號未亮"],
+                                "rootCauses": ["保險絲斷"],
+                                "solutionSteps": ["更換保險絲"],
+                                "notes": "注意安全"
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
