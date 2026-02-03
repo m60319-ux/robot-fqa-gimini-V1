517448481e9d816970dce06d58d080f0b147a4e8
m60319-ux <m60319@gmail.com>
2026-02-03 13:07:29 +0800

    Update data.zh.js

--- assets/data/data.zh.js
+++ assets/data/data.zh.js
@@ -5,34 +5,32 @@ window.FAQ_DATA_ZH = {
     },
     "categories": [
         {
-            "id": "CAT-01,硬體問題,SUB-01,馬達異常,Q-001,無法過電,燈號未,保險絲斷|{{img:assets/images/img_1769763508765.png}},更換保險絲|{{img:assets/images/img_1769763514401.png}},\"注意安全",
+            "id": "CAT-01",
+            "title": "硬體問題",
             "subcategories": [
                 {
+                    "id": "SUB-01",
+                    "title": "馬達異常",
                     "questions": [
                         {
+                            "id": "Q-001",
+                            "title": "無法過電",
                             "content": {
-                                "symptoms": [],
-                                "rootCauses": [],
-                                "solutionSteps": [],
-                                "keywords": []
-                            }
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "id": "{{img:assets/images/img_1769763517692.png}}",
-            "title": "AL001",
-            "subcategories": [
-                {
-                    "questions": [
-                        {
-                            "content": {
-                                "symptoms": [],
-                                "rootCauses": [],
-                                "solutionSteps": [],
-                                "keywords": []
+                                "symptoms": [
+                                    "燈號未"
+                                ],
+                                "rootCauses": [
+                                    "保險絲斷",
+                                    "{{img:assets/images/img_1769763508765.png}}"
+                                ],
+                                "solutionSteps": [
+                                    "更換保險絲",
+                                    "{{img:assets/images/img_1769763514401.png}}"
+                                ],
+                                "notes": "注意安全\n{{img:assets/images/img_1769763517692.png}}",
+                                "keywords": [
+                                    "AL001"
+                                ]
                             }
                         }
                     ]
