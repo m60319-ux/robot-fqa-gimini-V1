d083c38939385d962397bc7b8957a8375119f742
m60319-ux <m60319@gmail.com>
2026-02-03 13:23:08 +0800

    Update data.zh.js

--- assets/data/data.zh.js
+++ assets/data/data.zh.js
@@ -17,7 +17,7 @@ window.FAQ_DATA_ZH = {
                             "title": "無法過電",
                             "content": {
                                 "symptoms": [
-                                    "燈號未"
+                                    "燈號未亮"
                                 ],
                                 "rootCauses": [
                                     "保險絲斷",
@@ -34,34 +34,70 @@ window.FAQ_DATA_ZH = {
                             }
                         }
                     ]
-                }
-            ]
-        },
-        {
-            "id": "CAT-9742",
-            "title": "New",
-            "subcategories": [
+                },
                 {
-                    "id": "SUB-3842",
-                    "title": "New",
+                    "id": "SUB-02",
+                    "title": "驅動器異常",
                     "questions": [
                         {
-                            "id": "Q-6967",
-                            "title": "New",
+                            "id": "Q-002",
+                            "title": "驅動器輸出電流過大",
                             "content": {
                                 "symptoms": [
-                                    "321",
-                                    "{{img:assets/images/img_1770014988645.png}}"
+                                    "驅動器輸出電流過大"
+                                ],
+                                "rootCauses": [
+                                    "通常發生此狀況馬達的動力線(UVW)可能有問題"
+                                ],
+                                "solutionSteps": [],
+                                "notes": "",
+                                "keywords": [
+                                    "AL083"
+                                ]
+                            }
+                        }
+                    ]
+                },
+                {
+                    "id": "SUB-21",
+                    "title": "驅動器異常",
+                    "questions": [
+                        {
+                            "id": "Q-003",
+                            "title": "六軸AL830",
+                            "content": {
+                                "symptoms": [
+                                    "六軸J5、J6發生830，內部通訊異常"
                                 ],
                                 "rootCauses": [
                                     "123"
                                 ],
                                 "solutionSteps": [
-                                    "321"
+                                    "J5、J6 P3-09",
+                                    "0x3511改為0x3555"
                                 ],
-                                "notes": "123",
+                                "notes": "",
                                 "keywords": [
-                                    "AL001"
+                                    "AL830"
+                                ]
+                            }
+                        },
+                        {
+                            "id": "Q-004",
+                            "title": "AL044 驅動器功能使用率警告",
+                            "content": {
+                                "symptoms": [
+                                    "驅動器功能使用率警告\n\nDate        Time    Type    No.    Error Code    Description    描述    \n\n2024-12-09   12:50:19    Axis    Joint-5    AL802    Give motion command when the alarm occurs    有異警發生又下運動命令    \n\n2024-12-09   12:50:19    Axis    Joint-4    AL802    Give motion command when the alarm occurs    有異警發生又下運動命令    \n\n2024-12-09   12:50:19    Axis    Joint-3    AL802    Give motion command when the alarm occurs    有異警發生又下運動命令    \n\n2024-12-09   12:50:19    Axis    Joint-2    AL802    Give motion command when the alarm occurs    有異警發生又下運動命令    \n\n2024-12-09   12:50:19    Axis    Joint-6    AL044    Warning of Servo Drive function overload    驅動器功能使用率警告    \n\n2024-12-09   12:50:19    Axis    Joint-1    AL813    Axis error during interpretation of motion command    成員軸發生錯誤    \n\n2024-12-09   12:50:19    Group    1    E1.813    Axis error during interpretation of motion command    成員軸發生錯誤"
+                                ],
+                                "rootCauses": [
+                                    "該軸P2-66出廠預設值改為0x034。"
+                                ],
+                                "solutionSteps": [
+                                    "該軸P2-66出廠預設值改為0x034。"
+                                ],
+                                "notes": "",
+                                "keywords": [
+                                    "AL044"
                                 ]
                             }
                         }
@@ -70,31 +106,74 @@ window.FAQ_DATA_ZH = {
             ]
         },
         {
-            "id": "CAT-9741",
-            "title": "New",
+            "id": "CAT-02",
+            "title": "軟體問題",
             "subcategories": [
                 {
-                    "id": "SUB-3848",
-                    "title": "New",
+                    "id": "SUB-02",
+                    "title": "操作異常",
                     "questions": [
                         {
-                            "id": "Q-6969",
-                            "title": "New",
+                            "id": "Q-005",
+                            "title": "Modbus啟動專案無法運行",
                             "content": {
                                 "symptoms": [
-                                    "321",
-                                    "{{img:assets/images/img_1770014988645.png}}"
+                                    "用Modbus 0X228，專案還是無法啟動"
                                 ],
-                                "rootCauses": [
-                                    "123"
-                                ],
+                                "rootCauses": [],
                                 "solutionSteps": [
-                                    "321"
+                                    "查看DI 6、7是不是STOP，是的話就無法啟動"
                                 ],
-                                "notes": "123",
+                                "notes": "",
                                 "keywords": [
-                                    "AL001"
+                                    "Modbus"
                                 ]
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "id": "CAT-06",
+            "title": "",
+            "subcategories": [
+                {
+                    "id": "SUB-02",
+                    "title": "",
+                    "questions": [
+                        {
+                            "id": "Q-006",
+                            "title": "",
+                            "content": {
+                                "symptoms": [],
+                                "rootCauses": [],
+                                "solutionSteps": [],
+                                "notes": "",
+                                "keywords": []
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "id": "CAT-07",
+            "title": "",
+            "subcategories": [
+                {
+                    "id": "SUB-02",
+                    "title": "",
+                    "questions": [
+                        {
+                            "id": "Q-007",
+                            "title": "",
+                            "content": {
+                                "symptoms": [],
+                                "rootCauses": [],
+                                "solutionSteps": [],
+                                "notes": "",
+                                "keywords": []
                             }
                         }
                     ]
