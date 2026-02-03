9eab9c6464bfd2d24539509c7eb1991fedfe7381
m60319-ux <m60319@gmail.com>
2026-02-03 15:46:51 +0800

    Update data.zh.js

--- assets/data/data.zh.js
+++ assets/data/data.zh.js
@@ -21,24 +21,41 @@ window.FAQ_DATA_ZH = {
                                 ],
                                 "rootCauses": [
                                     "保險絲斷",
+                                    "",
                                     "{{img:assets/images/img_1769763508765.png}}"
                                 ],
                                 "solutionSteps": [
                                     "更換保險絲",
+                                    "",
                                     "{{img:assets/images/img_1769763514401.png}}"
                                 ],
-                                "notes": "注意安全\n{{img:assets/images/img_1769763517692.png}}",
+                                "notes": "AL001",
                                 "keywords": [
-                                    "AL001"
+                                    "注意安全\n{{img:assets/images/img_1769763517692.png}}"
                                 ]
                             }
                         }
                     ]
                 },
                 {
-                    "id": "SUB-21",
+                    "id": "SUB-02",
                     "title": "驅動器異常",
                     "questions": [
+                        {
+                            "id": "Q-002",
+                            "title": "驅動器輸出電流過大",
+                            "content": {
+                                "symptoms": [
+                                    "驅動器輸出電流過大"
+                                ],
+                                "rootCauses": [
+                                    "通常發生此狀況馬達的動力線(UVW)可能有問題"
+                                ],
+                                "solutionSteps": [],
+                                "notes": "AL083",
+                                "keywords": []
+                            }
+                        },
                         {
                             "id": "Q-003",
                             "title": "六軸AL830",
@@ -46,17 +63,14 @@ window.FAQ_DATA_ZH = {
                                 "symptoms": [
                                     "六軸J5、J6發生830，內部通訊異常"
                                 ],
-                                "rootCauses": [
-                                    "123"
-                                ],
+                                "rootCauses": [],
                                 "solutionSteps": [
                                     "J5、J6 P3-09",
+                                    "",
                                     "0x3511改為0x3555"
                                 ],
-                                "notes": "",
-                                "keywords": [
-                                    "AL830"
-                                ]
+                                "notes": "AL830",
+                                "keywords": []
                             }
                         },
                         {
@@ -64,7 +78,23 @@ window.FAQ_DATA_ZH = {
                             "title": "AL044 驅動器功能使用率警告",
                             "content": {
                                 "symptoms": [
-                                    "驅動器功能使用率警告\n\nDate        Time    Type    No.    Error Code    Description    描述    \n\n2024-12-09   12:50:19    Axis    Joint-5    AL802    Give motion command when the alarm occurs    有異警發生又下運動命令    \n\n2024-12-09   12:50:19    Axis    Joint-4    AL802    Give motion command when the alarm occurs    有異警發生又下運動命令    \n\n2024-12-09   12:50:19    Axis    Joint-3    AL802    Give motion command when the alarm occurs    有異警發生又下運動命令    \n\n2024-12-09   12:50:19    Axis    Joint-2    AL802    Give motion command when the alarm occurs    有異警發生又下運動命令    \n\n2024-12-09   12:50:19    Axis    Joint-6    AL044    Warning of Servo Drive function overload    驅動器功能使用率警告    \n\n2024-12-09   12:50:19    Axis    Joint-1    AL813    Axis error during interpretation of motion command    成員軸發生錯誤    \n\n2024-12-09   12:50:19    Group    1    E1.813    Axis error during interpretation of motion command    成員軸發生錯誤"
+                                    "驅動器功能使用率警告",
+                                    "",
+                                    "Date        Time    Type    No.    Error Code    Description    描述",
+                                    "",
+                                    "2024-12-09   12:50:19    Axis    Joint-5    AL802    Give motion command when the alarm occurs    有異警發生又下運動命令",
+                                    "",
+                                    "2024-12-09   12:50:19    Axis    Joint-4    AL802    Give motion command when the alarm occurs    有異警發生又下運動命令",
+                                    "",
+                                    "2024-12-09   12:50:19    Axis    Joint-3    AL802    Give motion command when the alarm occurs    有異警發生又下運動命令",
+                                    "",
+                                    "2024-12-09   12:50:19    Axis    Joint-2    AL802    Give motion command when the alarm occurs    有異警發生又下運動命令",
+                                    "",
+                                    "2024-12-09   12:50:19    Axis    Joint-6    AL044    Warning of Servo Drive function overload    驅動器功能使用率警告",
+                                    "",
+                                    "2024-12-09   12:50:19    Axis    Joint-1    AL813    Axis error during interpretation of motion command    成員軸發生錯誤",
+                                    "",
+                                    "2024-12-09   12:50:19    Group    1    E1.813    Axis error during interpretation of motion command    成員軸發生錯誤"
                                 ],
                                 "rootCauses": [
                                     "該軸P2-66出廠預設值改為0x034。"
@@ -72,10 +102,8 @@ window.FAQ_DATA_ZH = {
                                 "solutionSteps": [
                                     "該軸P2-66出廠預設值改為0x034。"
                                 ],
-                                "notes": "",
-                                "keywords": [
-                                    "AL044"
-                                ]
+                                "notes": "AL044\n驅動器||ABC",
+                                "keywords": []
                             }
                         }
                     ]
@@ -101,10 +129,8 @@ window.FAQ_DATA_ZH = {
                                 "solutionSteps": [
                                     "查看DI 6、7是不是STOP，是的話就無法啟動"
                                 ],
-                                "notes": "",
-                                "keywords": [
-                                    "Modbus"
-                                ]
+                                "notes": "Modbus",
+                                "keywords": []
                             }
                         }
                     ]
